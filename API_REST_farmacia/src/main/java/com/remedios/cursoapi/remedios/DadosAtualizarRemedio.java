921513f097be2a046bb8dd3a2e60d64be60d2b30
Cauã Conceição <95243694+CauaConceicao20@users.noreply.github.com>
2024-02-17 14:15:39 -0300

    Correção de anotações

--- API_REST_farmacia/src/main/java/com/remedios/cursoapi/remedios/DadosAtualizarRemedio.java
+++ API_REST_farmacia/src/main/java/com/remedios/cursoapi/remedios/DadosAtualizarRemedio.java
@@ -1,20 +1,18 @@
 package com.remedios.cursoapi.remedios;
 
-import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record DadosAtualizarRemedio(
 			@NotNull
 			Long id, 
 			
-			@NotBlank
+			
 			String nome, 
 			
-			@Enumerated
+			
 			Via via, 
 	
-			@Enumerated
+			
 			Laboratorio laboratorio) {
 	
 }
